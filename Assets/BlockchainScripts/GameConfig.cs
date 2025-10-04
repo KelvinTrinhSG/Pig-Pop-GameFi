476cca1cec3615a3843fcc779954414db91ef2be
KelvinTrinhSG <thkien85@Gmail.com>
2025-10-05 06:36:11 +0700

    Update Smart Contract
    Update Smart Contract

--- Assets/BlockchainScripts/GameConfig.cs
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -1,12 +1,12 @@
 ﻿public static class GameConfig
 {
     // Địa chỉ contract ERC721
-    public static readonly string NFTTokenGateContractAddress = "0xb2b7c30439072638989690E90aA8149d89F72780";
+    public static readonly string NFTTokenGateContractAddress = "0x6131C05d5cD75384Ed7C0747806dEB1CF613B060";
 
     // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
-    public static readonly string NFTVIPContractAddress = "0xa0a2F3fD7094cab1d20fa05a4A20c1751dd400E1";
+    public static readonly string NFTVIPContractAddress = "0x0b8b3beA7E04bc481fc900F74d3b1aEA1a81ba84";
 
     // Địa chỉ contract ERC20 - GEM Token
-    public static readonly string TokenGemContractAddress = "0x4FEeEAFCF9103Dbcc322f9b94440157657B0Dcd4";
+    public static readonly string TokenGemContractAddress = "0x73f08B28b0BF881051ab141E72F4F937b7c02C97";
 
 }
